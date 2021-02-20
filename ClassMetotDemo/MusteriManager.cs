7c52615231283df89e4ff84e9c79747ae48ca904
TUBA NUR YAŞA <l5216052@std.yildiz.edu.tr>
2021-02-20 12:21:54 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            musteri.Id = 1111111;
+            musteri.Ad = "Tuba Nur";
+            musteri.Soyad = "YASA";
+            Console.WriteLine("müsteri eklendi.");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("musteri Id =" + musteri.Id);
+            Console.WriteLine("musteri Ad = " + musteri.Ad);
+            Console.WriteLine("musteri Soyad= " + musteri.Soyad);
+        }
+
+        public void Sil()
+        {
+            Console.WriteLine("Musteri silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            Musteri[] musteriler = new Musteri[] { };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri);
+            musteriManager.Listele(musteri);
+            musteriManager.Sil();
+        }
+    }
+}
